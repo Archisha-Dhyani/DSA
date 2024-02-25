9fd02ee840ee6f73a65b1e8bd0128714f7cc9f8b
Archisha-Dhyani <archishadhyani8@gmail.com>
2024-02-26 01:31:50 +0530

    Day 06

--- PeakIndex.java
+++ PeakIndex.java
@@ -8,20 +8,20 @@ public class PeakIndex {
     public static int peakIndexInMountainArray(int[] arr) {
         int start=0;
         int end=arr.length-1;
-        int ind=-1;
         while(start<=end){
 
            int mid=start+(end-start)/2;
            if(arr[mid]>arr[mid+1]){
-            end=mid-1;
+            // this may be the wnswer , thats why end!=mid-1
+            end=mid;
            }
-           if(arr[mid+1]>arr[mid]){
-               ind=mid+1;
+           else{
+            
             start=mid+1;
            }
           
         }
-        return ind;
+        return start;
     }
     public static void main(String[] args) {
         int[] arr={0,10,15,2};
