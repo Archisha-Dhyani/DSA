afd1ae3b435be81cb9327b59726284647cb759f8
Archisha-Dhyani <archishadhyani8@gmail.com>
2024-02-25 09:10:57 +0530

    Day 05

--- PeakIndex.java
+++ PeakIndex.java
@@ -1,5 +1,31 @@
-
+/*
+ * LEETCODE 852. Peak Index in a Mountain Array
+ * Basically, greatest elemet in the array 
+ * 
+ */
 
 public class PeakIndex {
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        int ind=-1;
+        while(start<=end){
+
+           int mid=start+(end-start)/2;
+           if(arr[mid]>arr[mid+1]){
+            end=mid-1;
+           }
+           if(arr[mid+1]>arr[mid]){
+               ind=mid+1;
+            start=mid+1;
+           }
+          
+        }
+        return ind;
+    }
+    public static void main(String[] args) {
+        int[] arr={0,10,15,2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
     
 }
